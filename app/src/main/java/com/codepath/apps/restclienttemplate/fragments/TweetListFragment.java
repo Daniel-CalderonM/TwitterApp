2e076f37faa7c6c92750dfe139e53837684ae7b5
Daniel Calderon Manriquez <calderond@fb.com>
2017-07-05 16:10:56 -0700

    Moved over to fragments. Added layout for floating action button. Next step is to implement button functionality.

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/fragments/TweetListFragment.java
@@ -0,0 +1,104 @@
+package com.codepath.apps.restclienttemplate.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import com.codepath.apps.restclienttemplate.R;
+import com.codepath.apps.restclienttemplate.TweetAdapter;
+import com.codepath.apps.restclienttemplate.TwitterReply;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+
+/**
+ * Created by calderond on 7/3/17.
+ */
+
+public class TweetListFragment extends Fragment implements TweetAdapter.TweetAdapterListener{
+
+    public interface  TweetSelectedListener{
+        //handle tweet selection
+        public void onTweetSelected(Tweet tweet);
+    }
+
+    TweetAdapter tweetAdapter;
+    ArrayList<Tweet> tweets;
+    RecyclerView rvTweets;
+    ImageButton button;
+    //Request COde
+    private final int REQUEST_CODE = 20;
+    private SwipeRefreshLayout swipeContainer;
+    //Inflation happens inside onCreateView
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v2 = inflater.inflate(R.layout.item_tweet,container,false);
+
+        button = (ImageButton) v2.findViewById(R.id.btnReply);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getContext(),TwitterReply.class);
+                startActivity(i);
+            }
+        });
+        //INflate the layout
+        View v = inflater.inflate(R.layout.fragments_tweets_list,container, false);
+        //define button for reply
+        //find the RecyclerView
+        rvTweets = (RecyclerView) v.findViewById(R.id.rvTweet);
+        //init the arraylist (data source)
+        tweets = new ArrayList<>();
+        //construct the adapater from this datasource
+        tweetAdapter = new TweetAdapter(tweets,this);
+        //RecyclerView setup (layout manager, use adapter)
+        rvTweets.setLayoutManager(new LinearLayoutManager(getContext()));
+        //set the adapter
+        rvTweets.setAdapter(tweetAdapter);
+        return v;
+    }
+    public void addItems(JSONArray response){
+        //iterate through the JSON array
+        //for each entery, desirialize the JSON object
+        for(int i = 0 ;  i < response.length();i++){
+            //convert each object ot a Tweet model
+            //add that Tweet model to our data source
+            //notify the adapter that we have added an item
+            try{
+                Tweet tweet=  Tweet.fromJSON(response.getJSONObject(i));
+                tweets.add(tweet);
+                tweetAdapter.notifyItemInserted(tweets.size()-1);
+
+            }catch( JSONException e){
+                e.printStackTrace();
+            }
+        }
+    }
+    public void updateTimeline(Tweet tweet){
+        Log.i("Info2",tweet.getUser().getScreenName()+" ");
+        tweets.add(0,tweet);
+        tweetAdapter.notifyItemInserted(0);
+        rvTweets.scrollToPosition(0);
+    }
+    @Override
+    public void onItemSelected(View view, int position) {
+        Tweet tweet = tweets.get(position);
+        ((TweetSelectedListener)getActivity()).onTweetSelected(tweet);
+    }
+
+}
